73046f6a33559131d09dc7676dabf7a72bdae0a2
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-11-30 14:50:05 -0400

    Upgrade the README.md file

--- README.md
+++ README.md
@@ -1,10 +1,131 @@
-# Homepage Project
+# Homepage
 
-This repository implements [The Odin Project's](https://www.theodinproject.com/) Homepage 
-Project with Vanilla CSS, JavaScript, Jest & esbuild. For more information about this project, please visit:
+![Homepage](./readme-assets/screenshot-01.png)
 
-[https://www.theodinproject.com/lessons/node-path-advanced-html-and-css-homepage](https://www.theodinproject.com/lessons/node-path-advanced-html-and-css-homepage)
+This website is the submission for [The Odin Project: Homepage](https://www.theodinproject.com/lessons/node-path-advanced-html-and-css-homepage). 
 
-A live demo of this solution can be accessed here:
+The key features are:
 
-[https://mar-y-luna.web.app](https://mar-y-luna.web.app)
+- Modern Layout
+- Highly Accessible
+- Optimized Build
+- Cross-Platform Compatibility
+
+
+
+
+
+## Live Demo
+
+[Homepage](https://mar-y-luna.web.app/)
+
+
+
+
+
+## Getting Started
+
+### Requirements
+
+- GIT
+- NodeJS ^v21.0.0
+- NPM ^v10.2.0
+- Firebase CLI
+
+### Installation
+
+1) Clone the repository
+```bash
+git clone git@github.com:jesusgraterol/maryluna.git
+```
+
+2) Install the dependencies
+```bash
+npm install
+```
+
+
+
+
+## Usage
+
+1) Serve the app with esbuild
+
+```bash
+npm run serve
+```
+
+2) Access it through: `http://localhost:9000/`
+
+
+
+## Build Process
+
+1) Run esbuild for production
+```bash
+npm run build
+```
+
+
+
+## Deployment
+
+When a commit is pushed to the `main` branch, **Github Workflows** automatically pushes the new
+distribution to **Firebase Hosting**.
+
+
+
+
+
+## Built With
+
+- HTML5
+- CSS3
+- JavaScript
+- Jest
+- esbuild
+
+
+
+
+## Running the Tests
+
+```bash
+# Unit Tests
+npm run test:unit
+
+# Integration Tests
+npm run test:integration
+
+# E2E Tests
+N/A
+```
+
+
+
+
+
+## @TODO
+
+- Enable Multiple-Language Support
+
+
+
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+
+
+
+## Acknowledgments
+
+- [Google Fonts](https://fonts.google.com/icons)
+- [ESLint](https://eslint.org/)
+- [Jest](https://jestjs.io/)
+- [Babel](https://babeljs.io/)
+- [esbuild](https://esbuild.github.io/)
+- [Firebase](https://firebase.google.com/)
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,30 +1,221 @@
-(()=>{var d=class{#i;#e;#t;#d;#o;#s=!1;constructor(i,e,t){this.#i=i,this.#e=e,this.#t=document.getElementById(t),this.#t.addEventListener("click",o=>this.#a(o)),this.#d=document.getElementById(`${t}Open`),this.#o=document.getElementById(`${t}Close`),this.#d.addEventListener("click",()=>this.open()),this.#o.addEventListener("click",()=>this.close()),this.#i.addEventListener("keyup",o=>this.#r(o))}get isOpened(){return this.#s}#a(i){(i.target.id===this.#t.id||i.target instanceof HTMLAnchorElement||i.target.parentNode instanceof HTMLAnchorElement)&&this.close()}#r(i){i.key==="Escape"&&this.close()}open(){this.#s||(this.#e.style["overflow-y"]="hidden",this.#t.style.display="flex",this.#s=!0)}close(){this.#s&&(this.#e.style["overflow-y"]="scroll",this.#t.style.display="none",this.#s=!1)}},u=d;var m=u;var a=class{#i;#e;#t;#d;#o;constructor(i,e){this.#i=i,this.#e=document.getElementById(e),this.#t=document.getElementById(`${e}Title`),this.#d=document.getElementById(`${e}Close`),this.#o=document.getElementById(`${e}Content`),this.#d.addEventListener("click",()=>this.close()),this.#e.addEventListener("close",()=>this.#s())}show(i,e){this.#i.style["overflow-y"]="hidden",this.#t.innerText=i,this.#o.innerHTML=e,this.#e.showModal()}close(){this.#e.close(),this.#t.innerText="",this.#o.innerHTML=""}#s(){this.#i.style["overflow-y"]="scroll"}},p=a;var c=p;var r=class{#i;#e;constructor(i,e){this.#i=i,this.#e=document.getElementById(e),this.#e.addEventListener("click",()=>this.scrollToTop()),document.addEventListener("scroll",()=>this.#t())}#t(){this.#e.style.display=this.#i.scrollTop>400?"flex":"none"}scrollToTop(){this.#i.scrollTop=0}},h=r;var b=h;var n=class{#i;#e;#t;constructor(i,e,t){this.#i=i,this.#e=document.getElementById(e),this.#e.addEventListener("click",o=>this.#d(o)),this.#t=t}#d(i){let e=i.target.getAttribute(this.#t);e==="lessons"?this.#o():e==="rent"?this.#s():e==="accomodation"&&this.#a()}#o(){this.#i.show("Clases de Kiteboarding",`
+(() => {
+  // src/scripts/modules/sidenav/sidenav.js
+  var Sidenav = class {
+    // the instance of the document's element
+    #documentEl;
+    // the instance of the body's element
+    #bodyEl;
+    // the instance of the sidenav's element
+    #el;
+    // the button and close button elements
+    #openEl;
+    #closeEl;
+    // the state of the sidenav
+    #isOpened = false;
+    constructor(documentEl, bodyEl, sidenavID) {
+      this.#documentEl = documentEl;
+      this.#bodyEl = bodyEl;
+      this.#el = document.getElementById(sidenavID);
+      this.#el.addEventListener("click", (e) => this.#onElementClick(e));
+      this.#openEl = document.getElementById(`${sidenavID}Open`);
+      this.#closeEl = document.getElementById(`${sidenavID}Close`);
+      this.#openEl.addEventListener("click", () => this.open());
+      this.#closeEl.addEventListener("click", () => this.close());
+      this.#documentEl.addEventListener("keyup", (e) => this.#onKeyboardClickUp(e));
+    }
+    /* *********
+     * Getters *
+     ********* */
+    get isOpened() {
+      return this.#isOpened;
+    }
+    /* ******************************
+     * ELEMENT/KEYBOARD CLICK EVENT *
+     ****************************** */
+    /**
+     * Triggers whenever the sidenav element is clicked on and closes the sidenav if:
+     * - the background is clicked
+     * - a nav link is clicked
+     * @param {*} e
+     */
+    #onElementClick(e) {
+      if (e.target.id === this.#el.id || e.target instanceof HTMLAnchorElement || e.target.parentNode instanceof HTMLAnchorElement) {
+        this.close();
+      }
+    }
+    /**
+     * Triggers whenever a keyup event is emitted. The sidenav is closed if the escape key is pressed.
+     * @param {*} e
+     */
+    #onKeyboardClickUp(e) {
+      if (e.key === "Escape") {
+        this.close();
+      }
+    }
+    /* *************
+     * Controllers *
+     ************* */
+    /**
+     * Opens the sidenav if it isn't already opened.
+     */
+    open() {
+      if (!this.#isOpened) {
+        this.#bodyEl.style["overflow-y"] = "hidden";
+        this.#el.style.display = "flex";
+        this.#isOpened = true;
+      }
+    }
+    /**
+     * Closed the sidenav in case it is opened.
+     */
+    close() {
+      if (this.#isOpened) {
+        this.#bodyEl.style["overflow-y"] = "scroll";
+        this.#el.style.display = "none";
+        this.#isOpened = false;
+      }
+    }
+  };
+  var sidenav_default = Sidenav;
+
+  // src/scripts/modules/sidenav/index.js
+  var sidenav_default2 = sidenav_default;
+
+  // src/scripts/modules/dialog/dialog.js
+  var Dialog = class {
+    // the instance of the body's element
+    #bodyEl;
+    // the instance of the dialog element as well as its contents
+    #el;
+    #titleEl;
+    #closeEl;
+    #contentEl;
+    constructor(bodyEl, elementID) {
+      this.#bodyEl = bodyEl;
+      this.#el = document.getElementById(elementID);
+      this.#titleEl = document.getElementById(`${elementID}Title`);
+      this.#closeEl = document.getElementById(`${elementID}Close`);
+      this.#contentEl = document.getElementById(`${elementID}Content`);
+      this.#closeEl.addEventListener("click", () => this.close());
+      this.#el.addEventListener("close", () => this.#onDialogClose());
+    }
+    /**
+     * Builds and displays the modal based on the given content.
+     * @param {*} title
+     * @param {*} htmlContent
+     */
+    show(title, htmlContent) {
+      this.#bodyEl.style["overflow-y"] = "hidden";
+      this.#titleEl.innerText = title;
+      this.#contentEl.innerHTML = htmlContent;
+      this.#el.showModal();
+    }
+    /**
+     * Resets the content and closes the dialog.
+     */
+    close() {
+      this.#el.close();
+      this.#titleEl.innerText = "";
+      this.#contentEl.innerHTML = "";
+    }
+    /**
+     * Triggers whenever the dialog is closed. Takes care of renabling the scrolling functionality to
+     * the body
+     */
+    #onDialogClose() {
+      this.#bodyEl.style["overflow-y"] = "scroll";
+    }
+  };
+  var dialog_default = Dialog;
+
+  // src/scripts/modules/dialog/index.js
+  var dialog_default2 = dialog_default;
+
+  // src/scripts/modules/scroll-to-top-button/scroll-to-top-button.js
+  var ScrollToTopButton = class {
+    // the instance of the document's element
+    #documentEl;
+    // the instance of the button's element
+    #el;
+    constructor(documentEl, buttonID) {
+      this.#documentEl = documentEl;
+      this.#el = document.getElementById(buttonID);
+      this.#el.addEventListener("click", () => this.scrollToTop());
+      document.addEventListener("scroll", () => this.#onScrollChanges());
+    }
+    /**
+     * Triggers whenever the user scrolls on the page and determines if the button should be displayed
+     * or hidden.
+     */
+    #onScrollChanges() {
+      this.#el.style.display = this.#documentEl.scrollTop > 400 ? "flex" : "none";
+    }
+    /**
+     * Scrolls the window back to the top of the app.
+     */
+    scrollToTop() {
+      this.#documentEl.scrollTop = 0;
+    }
+  };
+  var scroll_to_top_button_default = ScrollToTopButton;
+
+  // src/scripts/modules/scroll-to-top-button/index.js
+  var scroll_to_top_button_default2 = scroll_to_top_button_default;
+
+  // src/scripts/modules/company-services/company-services.js
+  var CompanyServices = class {
+    // the instance of the dialog class
+    #dialog;
+    // the instance of the container's element
+    #el;
+    // the name of the attribute used to identify what dialogs to display
+    #dataDialogAttributeName;
+    constructor(dialog, servicesContainerID, dataDialogAttributeName) {
+      this.#dialog = dialog;
+      this.#el = document.getElementById(servicesContainerID);
+      this.#el.addEventListener("click", (e) => this.#onContainerElementClick(e));
+      this.#dataDialogAttributeName = dataDialogAttributeName;
+    }
+    /* *****************
+     * DIALOG HANDLERS *
+     ***************** */
+    /**
+     * Triggers when the "More info" button in a service card is clicked. Identifies which button
+     * was clicked and displays the appropriate dialog.
+     * @param {*} e
+     */
+    #onContainerElementClick(e) {
+      const dataDialog = e.target.getAttribute(this.#dataDialogAttributeName);
+      if (dataDialog === "lessons") {
+        this.#displayLessonsDialog();
+      } else if (dataDialog === "rent") {
+        this.#displayRentDialog();
+      } else if (dataDialog === "accomodation") {
+        this.#displayAccomodationDialog();
+      }
+    }
+    // Lessons Dialog
+    #displayLessonsDialog() {
+      this.#dialog.show("Clases de Kiteboarding", `
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
       </p>
-    `)}#s(){this.#i.show("Alquiler de Equipos",`
+    `);
+    }
+    // Rent Dialog
+    #displayRentDialog() {
+      this.#dialog.show("Alquiler de Equipos", `
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
       </p>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
       </p>
-    `)}#a(){this.#i.show("Estad\xEDa",`
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
-      </p>
+    `);
+    }
+    // Accomodation Dialog
+    #displayAccomodationDialog() {
+      this.#dialog.show("Estad\xEDa", `
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
       </p>
@@ -34,4 +225,56 @@
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
       </p>
-    `)}},g=n;var E=g;var l=class{#i;#e;#t;#d;#o;#s;#a;constructor(){this.#i=document.documentElement,this.#e=document.body,this.#t=document.getElementById("appContainer"),this.#d=new m(this.#i,this.#e,"mobileSidenav"),this.#o=new c(this.#e,"dialog"),this.#s=new b(this.#i,"scrollToTopButton"),this.#a=new E(this.#o,"services","data-dialog-id"),document.getElementById("currentYear").innerText=new Date().getFullYear()}},$=new l;})();
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus amet quidem debitis ad dolorum? Ipsum non pariatur maiores. Quod nobis aliquid aspernatur odit repudiandae omnis ab eius ut vel impedit?
+      </p>
+    `);
+    }
+  };
+  var company_services_default = CompanyServices;
+
+  // src/scripts/modules/company-services/index.js
+  var company_services_default2 = company_services_default;
+
+  // src/scripts/app.js
+  var App = class {
+    // the instance of the document's element
+    #documentEl;
+    // the instance of the body's element
+    #bodyEl;
+    // the instance of the app container's element
+    #appContainerEl;
+    // the instance of the mobile sidenav
+    #mobileSidenav;
+    // the instance of the dialog
+    #dialog;
+    // the instance of the scroll to top button
+    #scrollToTop;
+    // the instance of the company services
+    #companyServices;
+    constructor() {
+      this.#documentEl = document.documentElement;
+      this.#bodyEl = document.body;
+      this.#appContainerEl = document.getElementById("appContainer");
+      this.#mobileSidenav = new sidenav_default2(this.#documentEl, this.#bodyEl, "mobileSidenav");
+      this.#dialog = new dialog_default2(this.#bodyEl, "dialog");
+      this.#scrollToTop = new scroll_to_top_button_default2(this.#documentEl, "scrollToTopButton");
+      this.#companyServices = new company_services_default2(this.#dialog, "services", "data-dialog-id");
+      document.getElementById("currentYear").innerText = (/* @__PURE__ */ new Date()).getFullYear();
+    }
+  };
+  var app = new App();
+})();
+//# sourceMappingURL=app.js.map
